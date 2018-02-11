2c30f629e3630b71ca2826b87a3c2ab3c9b5ac0f
liam07he@gmail.com <HXH_Liam@Liams-MBP.hsd1.ca.comcast.net>
2018-02-11 15:30:40 -0800

    using socket to display form information without refreshing the page

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 let express = require('express');
 let path = require('path');
 let bodyParser = require('body-parser');
-let session = require('express-session');
 
 let app = express();
 
 app.use(bodyParser.urlencoded({extended : true}));
-app.use(session({secret: 'codingdojorocks'}));
 app.set('views', path.join(__dirname,'./views'));
 app.set('view engine', 'ejs');
 
@@ -15,17 +13,12 @@ app.get('/', function(req, res){
     res.render('index');
 })
 
-app.post('/form', function(req,res){
-    console.log(req.body);
-    req.session.name = req.body.name;
-    req.session.location = req.body.location;
-    req.session.language = req.body.language;
-    req.session.comments = req.body.comments;
-    res.redirect('/result')
+let server = app.listen(8000);
+let io = require("socket.io").listen(server);
+io.sockets.on('connection', function (socket) {
+    socket.on("submit", function (data) {
+        console.log(data);
+        data['num'] = Math.floor(Math.random() * 1001);
+        socket.emit('server', data);
+    })
 })
-
-app.get('/result',function(req,res){
-    res.render('result', {name:req.session.name, location:req.session.location, language:req.session.language, comments:req.session.comments })
-})
-
-app.listen(8000)
